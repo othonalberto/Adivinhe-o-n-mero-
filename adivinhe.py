0cc0e1767f4ae7c55c5dece4e633f4a9db22e195
othonalberto <othonalberto@gmail.com>
2016-07-31 21:01:18 -0300

    pep 8

--- adivinhe.py
+++ adivinhe.py
@@ -3,21 +3,22 @@
 import random
 import os
 
-chute = '' #se colocar chute igual 0 pode acontecer de automaticamente estar certo
+chute = ''  # se colocar chute igual 0 pode acontecer de automaticamente estar certo
 contador = 0
 nivel = 0
 numeros = [0]
 
+
 def dificuldade(nivel):
-	global numeros
-	if nivel == 1:
-		numeros.append(10)
-	elif nivel == 2:
-		numeros.append(50)
-	elif nivel == 3:
-		numeros.append(100)
-	else:
-		print("Erro. Rode o jogo novamente.")
+    global numeros
+    if nivel == 1:
+        numeros.append(10)
+    elif nivel == 2:
+        numeros.append(50)
+    elif nivel == 3:
+        numeros.append(100)
+    else:
+        print("Erro. Rode o jogo novamente.")
 
 os.system('clear')
 print("BEM-VINDO AO GAME 'ADIVINHE O NÚMERO!'\n\n")
@@ -27,24 +28,28 @@ dificuldade(level)
 sorteado = random.randint(numeros[0], numeros[1])
 os.system('clear')
 
-print("Dica: o número está entre {0} e {1}. Boa sorte! \n\n" .format(numeros[0], numeros[1]))
+print("Dica: o número está entre {0} e {1}. Boa sorte! \n\n" .format(
+    numeros[0], numeros[1]))
 input("Pressione ENTER para iniciar.")
 
 while chute != sorteado:
-	chute = int(input("Chute: "))
-	contador += 1
-	if chute in range(numeros[0], numeros[1] + 1):
-		if chute == sorteado:
-			os.system('clear')
-			print("Parabéns! Acertô, Miseravi.")
-		elif chute > sorteado:
-			print("Alto.\n")
-		else:
-			print("Baixo.\n")
-	else:
-		print("Hey, lembre-se que o número a ser adivinhado está entre {0} e {1}\n" .format(numeros[0], numeros[1]))
+    chute = int(input("Chute: "))
+    contador += 1
+    if chute in range(numeros[0], numeros[1] + 1):
+        if chute == sorteado:
+            os.system('clear')
+            print("Parabéns! Acertô, Miseravi.")
+        elif chute > sorteado:
+            print("Alto.\n")
+        else:
+            print("Baixo.\n")
+    else:
+        print(
+            "Hey, lembre-se que o número a ser adivinhado está entre {0} e {1}\n" .format(
+            	numeros[0], numeros[1]))
 
 if contador != 1:
-	print("Você deu {0} chutes para chegar ao número {1}." .format(contador, chute))
+    print("Você deu {0} chutes para chegar ao número {1}." .format(
+        contador, chute))
 else:
-	print("MENTIRA QUE VC ACERTOU DE PRIMEIRA!!!!!!1111111!!!!!")
+    print("MENTIRA QUE VC ACERTOU DE PRIMEIRA!!!!!!1111111!!!!!")
