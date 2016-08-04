471d401cf6d414dccd03dd32ecdd90b61bbaf3e5
othonalberto <othonalberto@gmail.com>
2016-08-04 00:51:52 -0300

    fix level problems

--- adivinhe.py
+++ adivinhe.py
@@ -18,38 +18,42 @@ def dificuldade(nivel):
     elif nivel == 3:
         numeros.append(100)
     else:
-        print("Erro. Rode o jogo novamente.")
+        nivel = "Erro, rode o jogo novamente!"
+    return nivel
 
 os.system('clear')
 print("BEM-VINDO AO GAME 'ADIVINHE O NÚMERO!'\n\n")
 level = int(input("Escolha a dificuldade: \n\n1-Fácil\n2-Médio\n3-Difícil\n"))
 
 dificuldade(level)
-sorteado = random.randint(numeros[0], numeros[1])
 os.system('clear')
 
-print("Dica: o número está entre {0} e {1}. Boa sorte! \n\n" .format(
-    numeros[0], numeros[1]))
-input("Pressione ENTER para iniciar.")
+if type(dificuldade(level)) == int:
+    sorteado = random.randint(numeros[0], numeros[1])
+    print("Dica: o número está entre {} e {}. Boa sorte! \n\n" .format(
+        numeros[0], numeros[1]))
+    input("Pressione ENTER para iniciar.")
 
-while chute != sorteado:
-    chute = int(input("Chute: "))
-    contador += 1
-    if chute in range(numeros[0], numeros[1] + 1):
-        if chute == sorteado:
-            os.system('clear')
-            print("Parabéns! Acertô, Miseravi.")
-        elif chute > sorteado:
-            print("Alto.\n")
+    while chute != sorteado:
+        chute = int(input("Chute: "))
+        contador += 1
+        if chute in range(numeros[0], numeros[1] + 1):
+            if chute == sorteado:
+                os.system('clear')
+                print("Parabéns! Acertô, Miseravi.")
+            elif chute > sorteado:
+                print("Alto.\n")
+            else:
+                print("Baixo.\n")
         else:
-            print("Baixo.\n")
-    else:
-        print(
-            "Hey, lembre-se que o número a ser adivinhado está entre {0} e {1}\n" .format(
-            	numeros[0], numeros[1]))
+            print(
+                "Hey, lembre-se que o número a ser adivinhado está entre {} e {}\n" .format(
+                    numeros[0], numeros[1]))
 
-if contador != 1:
-    print("Você deu {0} chutes para chegar ao número {1}." .format(
-        contador, chute))
+    if contador != 1:
+        print("Você deu {} chutes para chegar ao número {}." .format(
+            contador, chute))
+    else:
+        print("MENTIRA QUE VC ACERTOU DE PRIMEIRA!!!!!!1111111!!!!!")
 else:
-    print("MENTIRA QUE VC ACERTOU DE PRIMEIRA!!!!!!1111111!!!!!")
+    print(dificuldade(level))
