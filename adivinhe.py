444ef79cac78ac1e64d030b9546bf4c89197f925
othonalberto <othonalberto@gmail.com>
2016-09-13 15:02:57 -0300

    vetor de chutes

--- adivinhe.py
+++ adivinhe.py
@@ -7,9 +7,10 @@ chute = ''  # se colocar chute igual 0 pode acontecer de automaticamente estar c
 contador = 0
 nivel = 0
 numeros = [0]
+todos_chutes = []  # vetor para guardar todos os chutes dado pelo usuario
 
 
-def dificuldade(nivel):
+def dificuldade(nivel): #funcao que define a dificuldade
     global numeros
     if nivel == 1:
         numeros.append(10)
@@ -17,13 +18,27 @@ def dificuldade(nivel):
         numeros.append(50)
     elif nivel == 3:
         numeros.append(100)
+    elif nivel == 4:
+        numeros.append(1000)
     else:
         nivel = "Erro, rode o jogo novamente!"
     return nivel
 
+
+# função para mostrar se o chute foi o correto, está alto ou baixo.
+def posicao(chute, sorteado):
+    if chute == sorteado:
+        os.system('clear')
+        return("Parabéns! Acertô, Miseravi.")
+    elif chute > sorteado:
+        return("Alto.\n")
+    else:
+        return("Baixo.\n")
+
 os.system('clear')
 print("BEM-VINDO AO GAME 'ADIVINHE O NÚMERO!'\n\n")
-level = int(input("Escolha a dificuldade: \n\n1-Fácil\n2-Médio\n3-Difícil\n"))
+level = int(
+    input("Escolha a dificuldade: \n\n1-Fácil\n2-Médio\n3-Difícil\n4-Ultra\n"))
 
 dificuldade(level)
 os.system('clear')
@@ -36,24 +51,26 @@ if type(dificuldade(level)) == int:
 
     while chute != sorteado:
         chute = int(input("Chute: "))
+        todos_chutes.append(chute)
         contador += 1
         if chute in range(numeros[0], numeros[1] + 1):
-            if chute == sorteado:
-                os.system('clear')
-                print("Parabéns! Acertô, Miseravi.")
-            elif chute > sorteado:
-                print("Alto.\n")
-            else:
-                print("Baixo.\n")
+            print(posicao(chute, sorteado))
         else:
             print(
                 "Hey, lembre-se que o número a ser adivinhado está entre {} e {}\n" .format(
                     numeros[0], numeros[1]))
 
     if contador != 1:
-        print("Você deu {} chutes para chegar ao número {}." .format(
+        print("Você deu {} chutes para chegar ao número {}.\n" .format(
             contador, chute))
+        escolha = int(input("Deseja ver todos os chutes? \n\n1-Sim\n2-Não\n"))
+        os.system('clear')
+        # mostra todos os chutes dado pelo usuário
+        if escolha == 1:
+            for x in range(len(todos_chutes)):
+                print(todos_chutes[x])
     else:
-        print("MENTIRA QUE VC ACERTOU DE PRIMEIRA!!!!!!1111111!!!!!")
+        print("MENTIRA QUE VC ACERTOU DE PRIMEIRA!!!!!!1111111!!!!!\n")
+
 else:
     print(dificuldade(level))
